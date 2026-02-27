8625fbba3fd429e357b1da9feaa365c62d6ff975
TBKKSD <kitsadee16@gmail.com>
2026-02-27 13:30:19 +0700

    token api login_2

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ def new_todo(data):
                     done=data.get('done', False))
 
 @app.route('/api/todos/', methods=['POST'])
-@app.route('/api/todos/', methods=['POST'])
+@jwt_required()
 def add_todo():
     data = request.get_json()
     todo = new_todo(data)
@@ -51,6 +51,7 @@ def add_todo():
         return (jsonify({'error': 'Invalid todo data'}), 400)
 
 @app.route('/api/todos/<int:id>/toggle/', methods=['PATCH'])
+@jwt_required()
 def toggle_todo(id):
     todo = TodoItem.query.get_or_404(id)
     todo.done = not todo.done
@@ -59,6 +60,7 @@ def toggle_todo(id):
 
 
 @app.route('/api/todos/<int:id>/', methods=['DELETE'])
+@jwt_required()
 def delete_todo(id):
     todo = TodoItem.query.get_or_404(id)
     db.session.delete(todo)
@@ -66,6 +68,7 @@ def delete_todo(id):
     return jsonify({'message': 'Todo deleted successfully'})
 
 @app.route('/api/todos/<int:todo_id>/comments/', methods=['POST'])
+@jwt_required()
 def add_comment(todo_id):
     todo_item = TodoItem.query.get_or_404(todo_id)
 
@@ -86,6 +89,7 @@ def add_comment(todo_id):
 @click.argument("username")
 @click.argument("full_name")
 @click.argument("password")
+@jwt_required()
 def create_user(username, full_name, password):
     user = User.query.filter_by(username=username).first()
     if user:
